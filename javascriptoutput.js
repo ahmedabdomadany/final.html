8f6f1fc5eebff00a97fb050059926ff60882133f
ahmedabdomadany <ahmeed.abdo.al@gmail.com>
2024-07-21 08:24:45 +0300

    Add files via upload

---
+++ javascriptoutput.js
@@ -0,0 +1,29 @@
+var n1 ="holleo";
+var n2="word";
+function a(){
+console.log(n2+n2);
+}
+
+function b() {
+    var n1 =4;
+    var n2=3;
+console.log(n2+n2);
+}
+function c() {
+    var n1 =Object();
+    n1="ahmed";
+console.log("my name"+n1);
+}
+function d() {
+    var n1 =Object();
+    var n2=Object();
+    n1=1;
+    n2=2;
+console.log(n2+n2);
+}
+function e() {
+    var n1 =Object();
+    var n2=Object();
+    n3=n1.n2;
+console.log(n2+n2);
+}
